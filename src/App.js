5f3e3a2a3661364af1c7506d67360b26fc34c801
hunyshah1 <115942956+hunyshah1@users.noreply.github.com>
2023-02-12 09:03:58 +0000

    add nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 
+import Navbar from './navbar';
 function App() {
   return (
+    <>
+    <Navbar/>
     <div className="App">
+    
       <h1>
         now the data coming from react app
       </h1>
     </div>
+    </>
   );
 }
 
---
+++ src/navbar.js
@@ -0,0 +1,8 @@
+import './navbar.css'
+import Button from 'react-bootstrap/Button';
+const Navbar =()=>{
+    return(<>
+    <div className='navContainer' ><Button variant="primary">Primary</Button> </div>
+    </>)
+}
+export default Navbar;
